d4511a8b8708e8d1ac29bc07fd016ed522d46e13
JohnMerlino1235 <john.merlino1235@gmail.com>
2022-02-17 15:18:45 -0500

    Updated text for demo, displays size of at least 4 instead of 6

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from view.game_console_view import GameConsoleView
 #main.py
 board_size = input("Welcome to Reversi!\n"
                    "Local mode selected.\n"
-                   "Enter your game board size(at least 6, must be even): ")
+                   "Enter your game board size(at least 4, must be even): ")
 while int(board_size) % 2 == 1:
-    board_size = input("Invalid board size. Must be at least 6 and even: ")
+    board_size = input("Invalid board size. Must be at least 4 and even: ")
 
 print("Type \"exit\" at any time to exit the game.")
 game = Game(int(board_size))
