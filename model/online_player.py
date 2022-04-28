7c9725f1cd2eb31f49090a42a52e3604fe1a66db
John <john.merlino1235@gmail.com>
2022-04-28 12:58:58 -0400

    Added update_elo method to calculate elo based on opponents rating and game outcome.

--- model/online_player.py
+++ model/online_player.py
@@ -1,3 +1,5 @@
+import math
+
 from model.player import BasePlayer, player_color, Color
 
 
@@ -5,7 +7,7 @@ class OnlinePlayer(BasePlayer):
     def __init__(self, username, color: Color):
         super().__init__(color)
         self.username = username
-        #self.elo = self.get_elo
+        self.elo = 1500
     """
     def get_elo(self):
         # self.elo = pull from db
@@ -19,3 +21,10 @@ class OnlinePlayer(BasePlayer):
 
     def set_game(self, model):
         pass
+
+    def update_elo(self, opponent_rating, self_won):
+        probability = 1 * 1 // (1 + 1 * math.pow(10, 1 * (opponent_rating - self.elo) // 400)) # get probability of self winning
+        if self_won:
+            self.elo = 30 + self.elo * (1 - probability)
+        else:
+            self.elo = 30 + self.elo * (0 - probability)
