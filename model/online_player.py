072d0eeec4098738cb7e3a1fd4a3114b9ee5ffaf
John <john.merlino1235@gmail.com>
2022-04-27 21:07:56 -0400

    Added OnlineHumanPlayer

---
+++ model/online_player.py
@@ -0,0 +1,12 @@
+from model.game_model import Game
+from model.player import BasePlayer, Player
+from view.game_view import GameView
+
+
+class OnlinePlayer(BasePlayer):
+    def __init__(self, view: GameView, color: Player):
+        super().__init__(color)
+        self.view = view
+
+    def receive_move(self):
+        return self.view.request_move(None, None)
